e65d73a60ff59223ddfe72b4d80f855416b18627
Christian Lemieux <cjl5470@gmail.com>
2020-01-15 16:02:36 -0500

    adding placeholders for everything

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import GestureRecognizer from 'react-native-swipe-gestures';
+import { layout } from '../constants/Constants';
+
+
+// 'x-api-key': '3f7747b2-1ad1-489a-bdec-e39741f3a67c'
 
 class HomeScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        // initialize state
+    }
+
+    // onSwipeLeft
+
+    // onSwipeRight
+
+    // makeCatImageRequest
+
+    // componentDidMount
 
     render() {
         return (
@@ -14,16 +32,31 @@ class HomeScreen extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        alignItems: 'center',
+        height: layout.height,
+        width: layout.width,
         flex: 1,
-        backgroundColor: 'purple',
-        color: 'white',
-        flexDirection: 'column-reverse',
-        justifyContent: 'center',
     },
-    text: {
+    titleContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    imageContainer: {
+        flex: 2,
+        justifyContent: 'flex-start',
+    },
+    titleText: {
         color: 'white',
-        textAlign: 'center'
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    imageStyle: {
+        flex: 1,
+        height: undefined,
+        width: 300,
+        resizeMode: 'contain',
     }
 });
+
 
 export default HomeScreen;
--- containers/ProfileScreen.js
+++ containers/ProfileScreen.js
@@ -1,8 +1,25 @@
+
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
+// import Constants from 'expo-constants';
+// import * as Permissions from 'expo-permissions';
+
 import { colors } from '../constants/Constants';
 
 class ProfileScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        // initialize state
+    }
+
+    // getPermissionsAsync
+
+    // handleProfilePicPress
+
+    // handleRemovePicPress
+
     render(){
         return (
             <View style={styles.container}>
@@ -17,13 +34,27 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.mitBlue,
         color: 'white',
-        flexDirection: 'column-reverse',
+        flexDirection: 'column',
         justifyContent: 'center',
+        alignItems: 'center',
     },
     text: {
         color: 'white',
         textAlign: 'center'
-    }
+    },
+    profileImage: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        backgroundColor: '#86a7ff',
+        marginBottom: 32,
+    },
+    button: {
+        borderRadius: 3,
+        padding: 15,
+        width: 180,
+        marginBottom: 10,
+    },
 });
 
 export default ProfileScreen;
